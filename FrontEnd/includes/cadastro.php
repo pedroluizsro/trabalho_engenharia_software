d1e577c2c69da101cc4cef18d2e7cc4c1475ee25
pedro <pedroluizsroczynski@hotmail.com.br>
2015-04-30 22:27:54 -0300

    Cadastro - Backend

---
+++ FrontEnd/includes/cadastro.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pedro
+ * Date: 30/04/15
+ * Time: 21:31
+ */
+
+
+$nome = $_POST['nome'];
+$data = $_POST['data'];
+$descricao = $_POST['descricao'];
+$hashArquivo = md5((mt_rand(1,99))*(mt_rand(1,99))*(mt_rand(1,99)));
+
+var_dump($nome);
+var_dump($data);
+var_dump($descricao);
+var_dump($hashArquivo);
+
+$ext = strtolower(substr($_FILES['arquivo']['name'],-4));
+$destino = '../images/uploads/' . $hashArquivo . $ext;
+$arquivo_tmp = $_FILES['arquivo']['tmp_name'];
+move_uploaded_file( $arquivo_tmp, $destino  );
+
+var_dump($destino);
+
+?>
