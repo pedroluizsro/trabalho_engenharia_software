9109aced9da967f3a7dce4acf0ed71822389230b
pedro <pedroluizsroczynski@hotmail.com.br>
2015-05-05 22:23:43 -0300

    Cadastro - Backend

--- FrontEnd/includes/cadastro.php
+++ FrontEnd/includes/cadastro.php
@@ -12,14 +12,14 @@ $data = $_POST['data'];
 $descricao = $_POST['descricao'];
 $hashArquivo = md5((mt_rand(1,99))*(mt_rand(1,99))*(mt_rand(1,99)));
 
-var_dump($nome);
-var_dump($data);
-var_dump($descricao);
-var_dump($hashArquivo);
 
+/**
+ * $ext - Extenção do Arquivo.
+ * $destino - Destino do arquivo já com o seu nome randômico.
+ * $arquivo_tmp - Nome do arquivo temporário que será movido.
+ * move_uploaded_file() - Função que irá mover o arquivo para o diretório.
+ */
 $ext = strtolower(substr($_FILES['arquivo']['name'],-4));
 $destino = '../images/uploads/' . $hashArquivo . $ext;
 $arquivo_tmp = $_FILES['arquivo']['tmp_name'];
-move_uploaded_file( $arquivo_tmp, $destino  );
-
-var_dump($destino);
+move_uploaded_file($arquivo_tmp,$destino);
--- README.md
+++ README.md
@@ -7,3 +7,7 @@
     - Fernando;
     - Logan;
     - Pedro;
+
+
+- Observações:
+    - Os arquivos devem estar com as permissões de acordo para efetuar o Upload do arquivo, sabendo que o Apache move o arquivo até o diretório, é necessário que o usuário e grupo dono dos arquivos sejam o mesmo do Apache.
