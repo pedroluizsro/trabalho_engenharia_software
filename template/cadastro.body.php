75e4c9e55aa9f388cb1a66a3e9cfeb30aac962db
pedro <pedroluizsroczynski@hotmail.com.br>
2015-06-14 17:10:27 -0300

    Cadastro - Backend

--- template/cadastro.body.php
+++ template/cadastro.body.php
@@ -2,7 +2,7 @@
 <form class="formContainer" enctype="multipart/form-data" method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
     <input type="text" placeholder="Nome" name="nome">
     <input type="file" style="color: red;" name="arquivo" placeholder="Imagem" value="Selecione a imagem...">
-    <input type="date" placeholder="Data" name="data">
+    <input type="text" placeholder="Data" name="data" id="data">
     <textarea placeholder="Descritivo" name="descricao"></textarea>
     <input type="submit" value="Cadastrar">
 </form>
